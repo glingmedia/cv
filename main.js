d43fe1013eecf967e7f435f8b572dfb5f6c4bfac
glingmedia <jonas.glingert@medieinstitutet.se>
2020-11-09 21:31:12 +0100

    header

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-function
+//david header-script
+
+const header = document.querySelector('header');
+const menuIcon = document.getElementById('menu');
+
+menuIcon.onclick = function() {
+    if (header.style.height == '100%') {
+        header.style.height = null;
+    } else {
+        header.style.height = '100%';
+    }
+}
