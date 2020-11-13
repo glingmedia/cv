0c7756bca6abf444c0ded81bfd423331a936af88
glingmedia <jonas.glingert@medieinstitutet.se>
2020-11-13 13:28:12 +0100

    funkar ju inte

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 window.addEventListener('load', main);
 
 function main() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    const buttonAboutMe = document.getElementById('om');
+    buttonAboutMe.onclick = printAboutMe;
+}
+
+function printAboutMe() {
+    const om = document.getElementById('om');
+}
+
+/*
+window.addEventListener('load', main);
+
+
+
+function main() {
     houseAnimation();
 }
 
 function houseAnimation() {
-    setInterval(houseRotation, 5)
+    setInterval(houseFly, 8)
 }
 
-function houseRotation() {
+function houseFly() {
+    let left = 0;
     const house = document.querySelector('home');
+    left += 1;
+    house.style.left = left + '%';
+}    
+
+*/
