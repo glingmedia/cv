79f9249679142360697043b852dcbd3e0d3061c3
glingmedia <jonas.glingert@medieinstitutet.se>
2020-11-13 13:59:53 +0100

    knas

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ window.addEventListener('load', main);
 function main() {
     addEventListeners();
 }
+
+function addEventListeners() {
+    document.getElementById('portfolio').addEventListener('click', portfolioButton);
+}
+
+function portfolioButton(event) {
+    const button = event.target;
+}
